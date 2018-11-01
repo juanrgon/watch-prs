1ddd07fe8b80b80c600abadc738a2375c958e07c
Juan Gonzalez <jrg2156@gmail.com>
2018-11-01 14:21:26 -0400

    Improve UI

--- ui.go
+++ ui.go
@@ -15,31 +15,78 @@ func overwriteLine(text string) {
 	fmt.Print(clearAllAfterCursorSequence, text, "\r")
 }
 
+type state string
+const (
+	success state = state("success")
+	pending state = state("pending")
+	failure state = state("failure")
+)
+
 func printPullStatuses(client *github.Client, org string, repo string, pulls []*github.PullRequest) {
-	for _, pull := range pulls {
-		branch := pull.GetHead()
-		status := getPullRequestCombinedStatus(client, org, repo, branch)
-		success := "success"
-		fmt.Printf("%v: %s %s\n", coloredState(*status.State), branch.GetRef(), prism.InMagenta(pull.GetHTMLURL()))
-		if status.State != &success {
-			for _, status := range status.Statuses {
-				fmt.Printf("    %v: %v\n", coloredState(*status.State), prism.InCyan(*status.TargetURL))
-			}
+	for _, p := range pulls {
+		url := p.GetHTMLURL()
+		branch := p.GetHead()
+		name := branch.GetRef()
+
+		ciStatus := getPullRequestCombinedStatus(client, org, repo, branch)
+		mergeable := p.GetMergeable()
+		var overallStatus state
+		if mergeable && state(*ciStatus.State) == success {
+			overallStatus = success
+		} else {
+			overallStatus = failure
 		}
+
+		printPullBranch(name, url, overallStatus)
+		printPullMergeable(mergeable)
+		printCIStatus(ciStatus)
 		fmt.Println()
 	}
 }
 
-func coloredState(s string) (ds prism.DecoratedString) {
+func printPullBranch(name string, url string, status state) {
+	cn := coloredByState(name, status)
+	fmt.Printf("%s %s\n", cn, prism.InMagenta(url))
+}
+
+func printPullMergeable(m bool) {
+	s := "mergeable"
+	if m {
+		s = string(prism.InGreen(s))
+	} else {
+		s = string(prism.InRed(s))
+	}
+	fmt.Printf("    %s\n", s)
+}
+
+func printCIStatus(s *github.CombinedStatus) {
+	header := "ci-status"
+	combinedState := state(*s.State)
+
+	if state(combinedState) == success {
+		fmt.Printf("    %s\n", coloredByState(header, success))
+	}else{
+		fmt.Printf("    %s:\n", coloredByState(header, combinedState))
+		for _, status := range s.Statuses {
+			fmt.Printf("        %v: %v\n", coloredState(*status.State), prism.InCyan(*status.TargetURL))
+		}
+	}
+}
+
+func coloredByState(t string, s state) (ds prism.DecoratedString) {
 	switch s {
-	case "failure":
-		ds = prism.InRed(s)
-	case "success":
-		ds = prism.InGreen(s)
-	case "pending":
-		ds = prism.InYellow(s)
+	case failure:
+		ds = prism.InRed(t)
+	case success:
+		ds = prism.InGreen(t)
+	case pending:
+		ds = prism.InYellow(t)
 	}
 	return
+}
+
+func coloredState(s string) (ds prism.DecoratedString) {
+	return coloredByState(s, state(s))
 }
 
 func countDownTillNextRefresh(s int) {
